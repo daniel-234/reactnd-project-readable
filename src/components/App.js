a6256af7ea557cc3112f34b51dbcbdce745bf95b
Daniele <daniele.rb6@gmail.com>
2017-08-20 12:26:06 +0200

    refactor: Modify Project skeleton

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import * as ReadableAPI from './utils/ReadableAPI'
-import logo from './logo.svg';
-import './App.css';
+import * as ReadableAPI from '../utils/ReadableAPI'
+import logo from '../logo.svg';
+import '../App.css';
 
 class App extends Component {
   state = {
