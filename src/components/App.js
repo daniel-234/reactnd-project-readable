b328322420eb810821efbe4338b2ac005d0cd97c
Daniele <daniele.rb6@gmail.com>
2017-08-24 17:40:00 +0200

    refactor: Add form
    Added a form in place of the input in the main page to update the Redux store with a post with multiple fields.

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addPost } from '../actions';
+import '.././App.css';
 
 class App extends Component {
   submitPost = () => {
+
     this.props.displayPost({
       category: 'react',
       post: {
-        body: this.input.value
+        title: this.form.title,
+        author: this.form.author,
+        body: this.form.body
       }
     });
 
-    this.input.value = ''
+    this.form.reset();
+    return false;
   }
 
   render() {
@@ -19,16 +24,44 @@ class App extends Component {
 
     return (
       <div className="App">
-
-      <div className="App-header">
-          <h2>Readable App</h2>
+        <div className="App-header">
+          <h1>Readable App</h1>
         </div>
-        <input
-          type='text'
-          ref={(input) => this.input = input}
-          placeholder="Write a post about React"
-        />
-        <button onClick={this.submitPost}>Submit</button>
+          <h2>Add a post</h2>
+          <form
+            ref={(form) => this.form = form}>
+            <section>
+              <div>
+                <label htmlFor="title">Title:</label>
+                <input type='text' id='title' name='post-title' />
+              </div>
+              <div>
+                <label htmlFor='author'>Author:</label>
+                <input type='text' id='author' name='post-author' />
+              </div>
+              <div>
+                <label htmlFor='body'>Body:</label>
+                <textarea id='body' name='post-body'></textarea>
+              </div>
+            </section>
+            <div className='button'>
+              <button type='button' onClick={this.submitPost}>Submit</button>
+            </div>
+          </form>
+
+
+          {
+            /*
+              <input
+            type='text'
+            ref={(input) => this.input = input}
+            placeholder="Write a post about React"
+          />
+          <button onClick={this.submitPost}>Submit</button>
+            */
+          }
+
+
       </div>
     );
   }
