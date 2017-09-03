ce1fe03010fcc85c41aeed292f76406107ead964
Daniele <daniele.rb6@gmail.com>
2017-09-03 22:06:18 +0200

    feat: Install logger and compose enhancers
    Installed logger to log to the console the dispatched actions. Used Redux middleware for this purpose, as it would otherwise break the rule for Redux reducers to be pure functions.
    Added logger as the last middleware in chain to avoid it logging thunk and promise, instead of actual actions (as requested in the official documentation). Composed the enhancer middleware together with the Redux dev tools invocation.

--- src/index.js
+++ src/index.js
@@ -3,17 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import { fetchAllPosts } from './actions';
+import logger from 'redux-logger';
+// import { fetchAllPosts } from './actions';
 import reducer from './reducers';
 import { Provider } from 'react-redux';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
 	reducer,
-	applyMiddleware(
-		thunkMiddleware
+	composeEnhancers(
+		/*
+		 * Apply `middleware` for logging and to talk to
+		 * an asynchronous API.
+		 * Redux middleware provides a third-party extension
+		 * point between dispatching an action and the moment
+		 * it reaches the reducer.
+		 * The logger will show us informations every time an
+		 * action is dispatched.
+		 */
+		applyMiddleware(
+			thunkMiddleware,
+			logger
+		)
 	)
+
 	// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
